213a8d6ad61689eac6d8707b6c7a0275c5d5e6ae
venka <venka@host.docker.internal>
2022-08-18 09:58:05 +0530

    Collection Mapping

---
+++ BootProjJPA1-CollectionMapping/src/main/java/com/nt/BootProjJpa1CollectionMappingApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProjJpa1CollectionMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProjJpa1CollectionMappingApplication.class, args);
+	}
+
+}
